a288efcabd080751581ead7bab9f601c97a1f8f5
Pedro Rothen <p.rothen@outlook.cl>
2016-12-15 18:29:40 -0300

    Sources added

---
+++ PRViews/PRFakeTextFieldView.swift
@@ -0,0 +1,65 @@
+//
+//  HeaderView.swift
+//  Geyser
+//
+//  Created by Pedro on 05-04-16.
+//  Copyright © 2016 IQS. All rights reserved.
+//
+
+import UIKit
+//import CZPicker
+
+protocol PRFakeTextFieldViewDelegate{
+    func didTapView()
+}
+
+@IBDesignable class PRFakeTextFieldView: UIView {
+    @IBOutlet weak var textFieldNationality: UITextField!
+    @IBOutlet weak var viewArrow: UIView!
+    
+    var view: UIView!
+    
+    var nibName:String = "PRFakeTextFieldView"
+    
+    init(frame: CGRect, nibName: String) {
+        // 1. setup any properties here
+        
+        // 2. call super.init(frame:)
+        super.init(frame: frame)
+        
+        // 3. Setup view from .xib file
+        xibSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        // 1. setup any properties here
+        
+        // 2. call super.init(coder:)
+        super.init(coder: aDecoder)
+        
+        // 3. Setup view from .xib file
+        self.xibSetup()
+    }
+    
+    func xibSetup() {
+        self.view = loadViewFromNib()
+        
+        // use bounds not frame or it'll be offset
+        view.frame = bounds
+        
+        // Make the view stretch with containing view
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView {
+        //NSBundle.mainBundle()
+        let bundle = NSBundle.mainBundle()
+        let nib = UINib(nibName: self.nibName, bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        
+        return view
+    }
+}
+
