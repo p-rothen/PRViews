9bd53b93080c87b41173e8d568160fe6443a5b8a
Pedro Rothen <p.rothen@outlook.cl>
2016-12-15 16:49:13 -0300

    Initial Commit

---
+++ PRViews/PRViews.h
@@ -0,0 +1,19 @@
+//
+//  PRViews.h
+//  PRViews
+//
+//  Created by Pedro on 15-12-16.
+//  Copyright © 2016 Rothen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PRViews.
+FOUNDATION_EXPORT double PRViewsVersionNumber;
+
+//! Project version string for PRViews.
+FOUNDATION_EXPORT const unsigned char PRViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PRViews/PublicHeader.h>
+
+
