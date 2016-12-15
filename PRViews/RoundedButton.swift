0406b213c1b2b7903f9b34fd88ae26abf3a35cd0
Pedro Rothen <p.rothen@outlook.cl>
2016-12-15 18:11:57 -0300

    Sources added

---
+++ PRViews/RoundedButton.swift
@@ -0,0 +1,24 @@
+//
+//  RoundedButton.swift
+//  Doctor Cloud
+//
+//  Created by Pedro on 14-12-16.
+//  Copyright © 2016 IQS. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton : UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 3
+        
+        self.titleLabel?.font = UIFont.boldSystemFontOfSize(15)
+        
+        self.layer.shadowOpacity = 0.4;
+        self.layer.shadowRadius = 1;
+        self.layer.shadowColor = UIColor.blackColor().CGColor;
+        self.layer.shadowOffset = CGSizeMake(0.5, 0.5);
+        self.layer.masksToBounds = false
+    }
+}
